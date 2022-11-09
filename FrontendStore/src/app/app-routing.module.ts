325d42acc28a514e289af3f7c8753389edc3a89a
luisjavierluna <javierlunaluis@gmail.com>
2022-11-09 00:03:02 -0600

    59. Añadiendo los Productos

--- FrontendStore/src/app/app-routing.module.ts
+++ FrontendStore/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { UsComponent } from './us/us.component';
+import { ProductComponent } from './product/product.component';
 
 const routes: Routes = [
   {path: '', component: LandingPageComponent},
   {path: 'us', component: UsComponent},
+  {path: 'product', component: ProductComponent},
   {path: '**', redirectTo: ''},
 ]
 
